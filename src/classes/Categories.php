a09e591cec7783ed49997f51251f4ebcbdfd64fa
Hameed Abdulrahaman <hameedabdulrahamann@gmail.comgit config --global user.name Hameed>
2020-09-22 22:48:43 +0100

    update

--- src/classes/Categories.php
+++ src/classes/Categories.php
@@ -5,7 +5,7 @@ class Categories
     static function create($data)
     {
         extract($data);
-        $skey = SKey::session($session_id, $id);
+        $skey = SKey::session($session_id, $user_id);
         if ($skey) {
             return $skey;
         };
@@ -24,8 +24,28 @@ class Categories
     }
     static function show($id)
     {
-        $q = DB::query('SELECT * FROM categories WHERE id=:id', array(':id'=> $id));
+        $q = DB::query('SELECT * FROM categories WHERE id=:id', array(':id' => $id));
         return RHelper::response(true, 200, 'success', $q);
+    }
+    static function update($data)
+    {
+        extract($data);
+        $skey = SKey::session($session_id, $user_id);
+        if ($skey) {
+            return $skey;
+        };
+        $check = DB::query('SELECT name FROM categories WHERE name=:name', array(':name' => $name));
+        if (count($check) > 0) {
+            $name = empty($name) ? $check['name'] : $name;
+            $description = empty($description) ? $check['description'] : $description;
+
+            $q = DB::query('UPDATE categories SET  WHERE id=:id', array(
+                ':id' => $id,
+                ':name' => $name,
+                ':description' => $description
+            ));
+            return RHelper::response(true, 200, 'category was successfuly updated', $q);
+        }
     }
     static function destroy($id)
     {
